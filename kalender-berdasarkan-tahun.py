b824437229fa7b662b327bef36db39d0b860f8c2
Arya Pramudika <61450033+aryapramudika@users.noreply.github.com>
2021-04-30 20:07:56 +0700

    Create kalender-berdasarkan-tahun.py

---
+++ kalender-berdasarkan-tahun.py
@@ -0,0 +1,7 @@
+import calendar
+
+# Membuat variabel tahun yang berupa input angka integer
+tahun = int(input("Masukkan Tahun: "))
+
+#Mencetak kalender berdasarkan input tahun
+print(calendar.prcal(tahun))
